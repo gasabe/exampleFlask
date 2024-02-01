3340a9c51b2beb489aad6ed4e3a713ee2d824f4d
gasabe <abelardosgaston@gmail.com>
2024-02-01 16:27:58 -0300

    changes

--- App/app.py
+++ App/app.py
@@ -4,6 +4,13 @@ app= Flask(__name__)
 @app.route("/")
 def index():
     #return "<h1>aguante boquita papa- la mitad +1 </h1>"
-    return render_template('index.html')
+    cursos=["PHP","PYTHON","SQL","FLASK","KOTLIN"]
+    data={
+        "titulo":"index",
+        "bienvenida":"Bienvenido la cantidad de cursos que tenemos son ",
+        "cursos": cursos,
+        "numero_cursos":len(cursos)
+    }
+    return render_template('index.html',data=data)
 if __name__=="__main__":
     app.run(debug=True,port=5000)
